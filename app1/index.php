ae1ff823e7358ba9d812b6980f3562ebdd780b3a
hazem <hazemgmal434@gmail.com>
2020-05-15 12:03:08 +0200

    i payed the project

---
+++ app1/index.php
@@ -0,0 +1,102 @@
+<?php
+
+// $grade = 90;
+// switch($grade) {
+//     case 100:
+//         echo 'A+';
+//     break;
+
+//     case $grade > 90:
+//         echo 'A';
+//     break;
+//     case 90 :
+//         echo "A";
+//     break;
+
+//     case 85:
+//         echo 'B+';
+//     break;
+//     case 80:
+//         echo 'B';
+//     break;
+//     case 75:
+//         echo 'C';
+//     break;
+//     case $grade > 70;
+//     echo 'E';
+//     break;
+//     case 70:
+//         echo 'D';
+//     break;
+
+//     default:
+//     echo 'F';
+
+// }
+
+
+// $gread = 86;
+// if($gread  >= 90){
+//    print 'A+';
+// }elseif($gread > 85 && $gread < 90){
+//     echo "A";
+// }elseif($gread == 85){
+//     echo "B+";
+// }elseif($gread < 85 && $gread > 75){
+//     echo "B";
+// }elseif($gread <= 75 && $gread >=70){
+//     echo "c";
+// }elseif($gread < 70 && $gread >=65){
+//     echo 'D';
+// }elseif($gread < 65 && $gread >=50){
+//     echo "E";
+// }else{
+//     echo "F";
+// }
+
+  // ////////////////////////
+// $gread = 85;
+// switch($gread){
+//     case 100:
+//         echo 'A++';
+//     break;
+//     case $gread >= 90:
+//         echo 'A';
+//     break;
+//     case $gread >= 80:
+//         echo 'B';
+//     break;
+//     case $gread >= 75:
+//         echo 'C';
+//     break;
+//     case $gread >= 70:
+//         echo "D";
+//     break;
+//     case $gread >= 50:
+//         echo "E";
+//     break;
+//     default:
+//     echo "F";
+// }
+//////////////////////
+$degree = 80;
+switch($degree){
+    case $degree > 85:
+    echo "A";
+break;
+    case $degree > 75:
+        echo 'B';
+    break;
+    case $degree > 65:
+        echo "C";
+    break;
+    case $degree >= 50; 
+    echo "D";
+    break;
+
+    default:
+    echo "F";
+
+}
+
+?>
